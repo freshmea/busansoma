1bd8e270cdeab7d824d2b05ae9dab4235316f764
freshmea <freshmea@naver.com>
2024-04-17 19:49:01 +0900

    turtlemove

--- turtle_move/setup.py
+++ turtle_move/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'turtle_move'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob(os.path.join('launch', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
