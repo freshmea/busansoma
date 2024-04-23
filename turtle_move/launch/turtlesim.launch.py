3c3e75826436143ebf7f0f9a4ad5859fb02b6b4b
freshmea <freshmea@naver.com>
2024-04-23 16:44:25 +0900

    launch

--- turtle_move/launch/turtlesim.launch.py
+++ turtle_move/launch/turtlesim.launch.py
@@ -2,6 +2,7 @@ import os
 
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -11,7 +12,20 @@ def generate_launch_description():
                 default=os.path.join(get_package_share_directory("turtle_move"), "param", "turtlesim.yaml"))
     return LaunchDescription(
         [
-            Node(package="turtlesim", executable="turtlesim_node", output="screen"),
-            Node(package="turtle_move", executable="moveTurtleSim", output="screen"),
+            DeclareLaunchArgument(
+                "param_dir",
+                default_value=param_dir
+                ),
+            Node(
+                package="turtlesim",
+                executable="turtlesim_node",
+                parameters=[param_dir],
+                output="screen"
+                ),
+            Node(
+                package="turtle_move",
+                executable="moveTurtleSim",
+                output="screen"
+                ),
         ]
     )
